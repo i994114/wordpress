2ec6b47ff227609fd3701610b63c094b61399b88
i994114 <i994114@hotmail.com>
2022-08-23 11:14:23 +0900

    一部非推奨処理(get_author_nickname)を修正

--- loop.php
+++ loop.php
@@ -5,7 +5,7 @@ if(have_posts()):
     <article class="article-item">
         <h2 class="article-title"><a href="<?php the_permalink();  ?>"><?php the_title(); ?></a></h2>
         <h3 style="font-size:80%;">
-            <?php  the_author_nickname(); ?> <?php  the_time("Y年m月j日"); ?> <?php single_cat_title('カテゴリー：'); ?>
+            <?php  get_author_meta(); ?> <?php  the_time("Y年m月j日"); ?> <?php single_cat_title('カテゴリー：'); ?>
         </h3>
         <p class="article-body">
             <?php the_content();  ?>
