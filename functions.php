9ae336a3d303d7330c4905ac894f76879f01c1db
i994114 <i994114@hotmail.com>
2022-09-01 10:24:08 +0900

    Lesson 08「売れるテンプレートを作ろう！〜関数編その１〜」 Q&A送信時のコード

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
+
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG_LOG', true );
+
 //カスタムヘッダー画像の設置
 $custom_header_defaults = array(
-    'default-image' =>get_bloginfo('template_url').'/images/headers/logo.png',
+    'default-image' => get_bloginfo('template_url').'/images/headers/logo.png',
     'header-text' => false, //ヘッダー画面上にテキストをかぶせる
 );
 //カスタムヘッダー機能を有効にする
@@ -113,17 +117,16 @@ function save_custom_postdata($post_id){
     if(isset($_POST['img-top'])){
         $img_top = $_POST['img-top'];
     }
-
-    //カスタムフィールドに入力された情報を取り出す
-    if(isset($_POST['about_msg'])){
-        $about_msg = $_POST['about_msg'];
-    }
     if ($img_top != get_post_meta($post_id, 'img-top', true)) {
         update_post_meta($post_id, 'img-top', $img_top);
     } else {
         delete_post_meta($post_id, 'img-top', get_post_meta($post_id, 'img-top', true));
     }
 
+    //about
+    if(isset($_POST['about_msg'])){
+        $about_msg = $_POST['about_msg'];
+    }
     //内容がかわっていた場合、保存していた情報を更新する
     if ($about_msg != get_post_meta($post_id, 'about',true)) {
         update_post_meta($post_id, 'about', $about_msg);
@@ -138,27 +141,121 @@ function save_custom_postdata($post_id){
         }
 
         if ($recruit_data != get_post_meta($post_id, 'recruit_info'.$i, true)) {
-            var_dump($_POST['recruit1']);
-            var_dump($_POST['recruit2']);
-            var_dump($_POST['recruit3']);
-            var_dump($_POST['recruit4']);
-            var_dump($_POST['recruit5']);
-            var_dump($_POST['recruit6']);
-            var_dump($_POST['recruit7']);
             update_post_meta($post_id, 'recruit_info'.$i, $recruit_data);
         } else if ($recruit_data != '') {
             delete_post_meta($post_id, 'recruit_info'.$i, get_post_meta($post_id, 'recruit_info'.$i, true));
         }
+    }
+    //map
+    if (isset($_POST['map'])) {
+        $map = $_POST['map'];
+    }
+    if ($map != get_post_meta($post_id, 'map', true)) {
+        update_post_meta($post_id, 'map', $map);
+    } else {
+        delete_post_meta($post_id, get_post_meta($post_id, 'map', true));
+    }
+}
+//-----------------
+//カスタムウィジェット
+//-----------------
+//ウィジェットエリアを作成する関数がどれなのかを登録する
+add_action('widgets_init', 'my_widgets_area');
+//add_action( 'widgets_init', function(){register_widget( 'my_widgets_area' );});
+//ウィジェット自体の作成するクラスがどれなのかを登録する
+//add_action('widgets_init', create_function('', 'return register_widget("my_widgets_item1");'));
+//memo:上がサンプル。PHP 5.3 以上から、ウィジェット作成する際には下記の書き方に仕様が変更されています。
+add_action( 'widgets_init', function(){register_widget( 'My_Widget' );});
 
-        //map
-        if (isset($_POST['map'])) {
-            $map = $_POST['map'];
-        }
-        if ($map != get_post_meta($post_id, 'map', true)) {
-            update_post_meta($post_id, 'map', $map);
-        } else {
-            delete_post_meta($post_id, get_post_meta($post_id, 'map', true));
-        }
+//ウィジェットエリアを作成する
 
+function my_widgets_area() {
+    register_sidebar(array(
+        'name' => 'メリットエリア', //memo:管理画面上の表示名
+        'id' => 'widget_merit',
+        'before_widget' =>'<div>',  //実際のウィジェットエリアを囲むhtmlタグの指定
+        'after_widget' => '</div>'
+    ));
+}
+
+//ウィジェット自体を作成する
+class My_Widget extends WP_Widget 
+{
+    //初期化(管理画面で表示するウィジェットの名前を設定する)
+
+    public function __construct() {
+        parent::__construct(false, $name = 'メリットウィジェット');
+    }
+
+/*
+    public function My_Widget() {
+        parent::WP_Widget(false, $name='メリットウィジェット');
+        //parent::__construct(false, $name='メリットウィジェット');
+    }
+*/
+
+    //ウィジェットの入力文字を作成する処理
+    //memo:$instanceは、WordPressからの情報を格納するための変数らしい
+    function form($instance)
+    {
+        //入力された情報をサニタイズして変数へ格納
+        $title = esc_attr($instance['title']);
+        $body = esc_attr($instance['body']);
+?>
+
+        <p>
+            <!-- memo:for属性は、ID属性を指定する属性のこと -->
+            <label for="<?php echo $this->get_field_id('title'); ?>">
+                <?php echo 'タイトル'; ?>
+            </label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text"
+             value="<?php echo $title; ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('body'); ?>">
+                <?php echo '内容:'; ?>
+            </label>
+            <textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('body'); ?>" name="<?php echo $this->get_field_name('body'); ?>">
+                <?php echo $body; ?>
+            </textarea>
+        </p>
+<?php 
+    }
+    //ウィジェットに入力された情報を保存する処理
+
+    function update($new_instance, $old_instance){
+        $instance = $old_instance;
+        $instance['title'] = strip_tag($new_instance['title']); //これもサニタイズ(php,htmlタグを取り除く)
+        //$instance['title']( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['body'] = trim($new_instance['body']);    //先頭と最後尾の空白を取り除く
+        //$instance['body']( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+
+        return $instance;
+    }
+
+    //管理画面から入力されたウィジェットを画面に表示する処理
+    function widget($args, $instance)
+    {
+        //配列を変数に展開
+        //memo:今回はつかわないが、extractは、連想配列を変数に変換するもの(連想配列のキーが変数になり、値はvalueのものが入る)
+        extract($args);
+
+        //ウィジェットから入力された情報を取得
+        $title = apply_filters('widget_title', $instance['title']);
+        $body = isset($instance['body'])?esc_attr($instance['body']):'';
+        //$body = apply_filters('widget_body', $instance['body']);
+ 
+        //ウィジェットから入力された情報がある場合、htmlを表示する
+        if ($title) {
+?>
+            <section class="panel">
+                <h2><?php echo $title; ?></h2>
+                <p>
+                    <?php echo $body; ?>
+                </p>
+            </section>
+<?php 
+        }
     }
 }
--- single.php
+++ single.php
@@ -1,19 +1,9 @@
+<!-- ヘッダー -->
 <?php get_header(); ?>
-	<body>
-		<header class="site-width">
-			<h1><a href="index.html">WEBUKATU</a></h1>
-			<nav id="top-nav">
-				<ul>
-					<li><a href="index.html">HOME</a></li>
-					<li><a href="http://webukatu.com/sample/html_practice/#about">ABOUT</a></li>
-					<li><a href="http://webukatu.com/sample/html_practice/#merit">MERIT</a></li>
-					<li><a href="http://webukatu.com/sample/html_practice/#recruit">RECRUIT</a></li>
-					<li><a href="contact.html">CONTACT</a></li>
-					<li><a href="map.html">INFO</a></li>
-					<li><a href="blog_list.html">BlOG</a></li>
-				</ul>
-			</nav>
-		</header>
+
+<!-- メニュー -->
+<?php get_template_menu('content', 'menu'); ?>
+
 		<div id="main">
 
 			<!-- blog_list -->
