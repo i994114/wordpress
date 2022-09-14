4dcd8e5b8e930d995698cffb691278c57624521d
i994114 <i994114@hotmail.com>
2022-09-15 00:53:55 +0900

    微修正とやり残し項目を追記

--- single.php
+++ single.php
@@ -2,8 +2,8 @@
 <?php get_header(); ?>
 
 <!-- メニュー -->
-<?php get_template_menu('content', 'menu'); ?>
-
+<?php get_template_part('content', 'menu'); ?>
+    <!-- やり残し：サイドバーが崩れるのと、両端記事のページネーションが狂う点 -->
 		<div id="main">
 
 			<!-- blog_list -->
@@ -16,7 +16,7 @@
                     <?php while (have_posts()) : the_post(); ?>
                             <article class="article-item">
                                 <h2 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                                <h3 style="font-size:80%;"><?php get_author_meta(); ?> <?php the_time("Y年m月j日"); ?> <?php single_cat_title('カテゴリー: '); ?></h3>
+                                <h3 style="font-size:80%;"><?php the_author_meta(); ?> <?php the_time("Y年m月j日"); ?> <?php single_cat_title('カテゴリー: '); ?></h3>
                                 <p class="article-body">
                                     <?php the_content(); ?>
                                 </p>
@@ -26,18 +26,19 @@
                     <div class="pagenation">
 						<ul>
                             <!-- 前のページへいくのと、次のページへいくリンク。2つ目の引数はその文字列 -->
+                            <!-- ここはとある1記事の下に表示されるので、いつものページネーションではなく、このようになっている -->
                             <li class="prev"><?php previous_post_link('%link', 'PREV'); ?></li>
                             <li class="next"><?php next_post_link('%link', 'NEXT'); ?></li>
 						</ul>
 					</div>
 
                     <!-- comment -->
-                    <?php comments_template(); ?>
+                    <?php comment_form(); ?>
                 <?php else : ?>
 					<h2 class="title">記事がみつかりませんでした</h2>
                     <p>検索でみつかるかもしれません</p>
                     <?php get_search_form(); ?>
-                <?php endif ?>
+                <?php endif; ?>
 
                 <!-- サイドバー -->
                 <?php get_sidebar(); ?>
